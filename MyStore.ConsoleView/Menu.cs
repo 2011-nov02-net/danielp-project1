39789b2a6395fe3c060317959c14d658240dda40
peter346 <peter346@wwu.edu>
2020-11-06 14:43:19 -0800

    Created View Project

---
+++ MyStore.ConsoleView/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.ConsoleView
+{
+    interface Menu
+    {
+        //Display
+
+        //accepted inputs
+    }
+}
---
+++ MyStore.ConsoleView/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyStore.ConsoleView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
