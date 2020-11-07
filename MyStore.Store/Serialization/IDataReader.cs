f37a3e38417cc67d257cc432e1de512c83a840ae
peter346 <peter346@wwu.edu>
2020-11-06 19:15:41 -0800

    Created Planned Serialization

---
+++ MyStore.Store/Serialization/IDataReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Store.Serialization
+{
+    /// <summary>
+    /// Classes responsible for reading data into objects
+    /// </summary>
+    interface IDataReader
+    {
+    }
+}
---
+++ MyStore.Store/Serialization/IDataWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Store.Serialization
+{
+    /// <summary>
+    /// Classes responsible for saving data
+    /// </summary>
+    interface IDataWriter
+    {
+    }
+}
