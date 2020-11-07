52d0441f8e1c33874b404473375c815a007c9188
peter346 <peter346@wwu.edu>
2020-11-07 13:21:13 -0800

    REFACTOR: Renamed Location Holder Class

---
+++ MyStore.Store/Locations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Store
+{
+    //todo: singleton
+    class Locations
+    {
+        List<Location> stores;
+
+        private Locations _instance;
+
+        public Locations Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    return new Locations();
+                } else
+                {
+                    return _instance;
+                }
+            }
+        }
+
+
+        private Locations()
+        {
+            stores = new List<Location>();
+        }
+    }
+}
