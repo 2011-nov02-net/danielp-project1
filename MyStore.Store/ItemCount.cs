9c53e7af5c04d00adcd460c9a389d67bee8650f6
peter346 <peter346@wwu.edu>
2020-11-07 14:22:50 -0800

    Added wrapper for item-amount pairs

---
+++ MyStore.Store/ItemCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MyStore.Store
+{
+    public struct ItemCount
+    {
+        public int Count { get; }
+        public StoreCatalogue.Item ThisItem { get; }
+
+        public ItemCount(int count, string itemName)
+        {
+            ThisItem = StoreCatalogue.Instance.GetItem(itemName);
+            Count = count;
+        }
+    }
+}
