1471e74ba20ae7cbee3d6af7a65d5a8a16ce7b19
peter346 <peter346@wwu.edu>
2020-11-07 21:17:27 -0800

    Started Tests for Catalogue

---
+++ MyStore.Store.Tests/CatalogueTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using MyStore.Store;
+using static MyStore.Store.StoreCatalogue;
+using System.Diagnostics;
+using Xunit.Sdk;
+
+namespace MyStore.Store.Tests
+{
+    public class CatalogueTests
+    {
+        [Fact]
+        public void RegisterValidItem()
+        {
+            //arrange
+            string itemname = "TestItem";
+            float itemcost = 10;
+            StoreCatalogue.Instance.RegisterItem(itemname, itemcost);
+            //act 
+            Item testitem = StoreCatalogue.Instance.GetItem(itemname);
+
+            //assert
+            Assert.NotNull(testitem);
+            Assert.Equal(itemname, testitem.name);
+            Assert.Equal(itemcost, testitem.cost);
+
+        }
+
+        [Fact]
+        public void RegisterInvalidItem()
+        {
+            //arrange
+            string itemname = "testitem2";
+            StoreCatalogue.Instance.RegisterItem(itemname, 0);
+
+            //act
+            //assert
+
+            //todo make this more specific
+            Assert.ThrowsAny<Exception>(()=>StoreCatalogue.Instance.RegisterItem(itemname, 0));
+        }
+    }
+}
