2b42453c3431bc62db58d82561447a380653ebaf
peter346 <peter346@wwu.edu>
2020-11-08 12:25:13 -0800

    REFACTOR: Pulled namestruct out of Customer

--- MyStore.Store/Customer.cs
+++ MyStore.Store/Customer.cs
@@ -5,17 +5,16 @@
         //order history
         OrderHistory orderHistory;
 
-        public struct Name
-        {
-            string First;
-            char? MiddleInitial;
-            string Last;
-        };
-
         readonly Name CustomerName;
 
         //NOTE: optional req
         #nullable enable
-        Location? DefaultStore;
+        Location? DefaultStore = null;
+
+        public Customer(Name name, Location? DefaultStore = null)
+        {
+            CustomerName = name;
+            this.DefaultStore = DefaultStore;
+        }
     }
 }
---
+++ MyStore.Store/Name.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Store
+{
+    public struct Name
+    {
+        public string First { get; }
+        public char? MiddleInitial { get; }
+        public string Last { get; }
+
+        public Name(string first, string last)
+        {
+            First = first;
+            Last = last;
+            MiddleInitial = null;
+        }
+
+        public Name(string first, string last, char middle)
+        {
+            First = first;
+            Last = last;
+            MiddleInitial = middle;
+        }
+    };
+}
