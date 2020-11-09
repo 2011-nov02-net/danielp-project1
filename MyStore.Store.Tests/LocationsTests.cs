733ead964d60f58db3fb7d2b99e4b88186154baf
peter346 <peter346@wwu.edu>
2020-11-08 17:01:06 -0800

    Started filling in and testing Locations

---
+++ MyStore.Store.Tests/LocationsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MyStore.Store;
+using System.Diagnostics;
+
+namespace MyStore.Store.Tests
+{
+    public class LocationsTests
+    {
+
+        [Fact]
+        public void RegisterLocation()
+        {
+            //Arrange
+            string newlocname = "TestLocation0";
+            Assert.True(LocationDoesntExistYet(newlocname));
+            //Act
+            Location newloc = Locations.Instance.RegisterLocation(newlocname);
+            //Assert
+            Assert.NotNull(newloc);
+            Assert.Equal(newlocname, newloc.Where);
+        }
+
+
+        [Fact]
+        public void GetRealLocation()
+        {
+            //arrange
+            string locname = "TestLocation1";
+            Assert.True(LocationDoesntExistYet(locname));
+            Location mylocation = Locations.Instance.RegisterLocation(locname);
+            //act
+            Location returnedLoc = Locations.Instance.GetLocation(locname);
+
+            //assert
+            Assert.Equal(mylocation, returnedLoc);
+        }
+
+        [Fact]
+        public void FailGetFakeLocation()
+        {
+            //arrange
+            string locname = "TestLocation2";
+            Assert.True(LocationDoesntExistYet(locname));
+
+            //act
+            //assert
+            Assert.ThrowsAny<ArgumentException>(() => Locations.Instance.GetLocation(locname));
+
+        }
+
+
+        private Boolean LocationDoesntExistYet(string locname)
+        {
+            Boolean doesnotexist = true;
+            foreach (Location loc in Locations.Instance.Stores)
+            {
+                if (loc.Where == locname)
+                {
+                    doesnotexist = false;
+                }
+            }
+
+            return doesnotexist;
+        }
+    }
+}
--- MyStore.Store/Locations.cs
+++ MyStore.Store/Locations.cs
@@ -5,30 +5,103 @@ using System.Text;
 namespace MyStore.Store
 {
     //singleton
-    class Locations
+    public class Locations
     {
-        List<Location> stores;
+        private Dictionary<string, Location> _stores;
+
+        public IReadOnlyCollection<Location> Stores 
+        {
+            get
+            {
+                return _stores.Values;
+            } 
+        }
 
         private static Locations _instance;
 
+        /// <summary>
+        /// Accsess the Locations list.
+        /// </summary>
+        /// <remarks>
+        /// Used because this is a singleton class.
+        /// </remarks>
         public static Locations Instance
         {
             get
             {
                 if (_instance is null)
                 {
-                    return new Locations();
-                } else
-                {
-                    return _instance;
+                    _instance = new Locations();
                 }
+                return _instance;
             }
         }
 
-
         private Locations()
         {
-            stores = new List<Location>();
+            _stores = new Dictionary<string, Location>();
+        }
+
+
+
+        /// <summary>
+        /// Create a location and add it to the list of known locations.
+        /// </summary>
+        /// <param name="newLocation">The name of a new store location</param>
+        /// <returns>The same location passed in, or throws an exception</returns>
+        /// <remarks>
+        /// This is intended for external use, to create a location, and return a reference too it to 
+        /// fill out the stock of the location. 
+        /// </remarks>
+        public Location RegisterLocation(String locatedAt)
+        {
+            Location newlocation;
+            if (_stores.ContainsKey(locatedAt))
+            {
+                //TODO: consider changing exception
+                throw new ArgumentException("Location Already Registered");
+            }
+            else
+            {
+                newlocation = new Location(locatedAt);
+                _stores.Add(locatedAt, newlocation);
+            }
+
+            return newlocation;
+        }
+
+
+        /// <summary>
+        /// Add a newly created location to the list of known locations.
+        /// </summary>
+        /// <param name="newLocation">A Location object that's been created</param>
+        /// <returns>The same location passed in, or throws an exception</returns>
+        /// <remarks>
+        /// This is intended for internal use mostly, since only internal functions can create Locations. 
+        /// </remarks>
+        public Location RegisterLocation(Location newLocation)
+        {
+            if(_stores.ContainsKey(newLocation.Where))
+            {
+                throw new ArgumentException("Location Already Registered");
+            } else
+            {
+                _stores.Add(newLocation.Where, newLocation);
+            }
+
+            return newLocation;
+        }
+
+
+
+        /// <summary>
+        /// Get the Location corasponding to the name
+        /// </summary>
+        /// <param name="where">the name of the location</param>
+        /// <returns>The location from the list of locations with the given key.</returns>
+        public Location GetLocation(string where)
+        {
+            throw new Exception();
         }
     }
 }
