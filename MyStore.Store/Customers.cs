7788a536a3ea82fbbfc4922125283909f0aab482
peter346 <peter346@wwu.edu>
2020-11-09 16:49:29 -0800

    Documentation for customers.

--- MyStore.Store/Customers.cs
+++ MyStore.Store/Customers.cs
@@ -9,7 +9,7 @@ namespace MyStore.Store
     /// Tracks all the customers in the system.
     /// </summary>
     /// <remarks>
-    /// Singleton
+    /// This is a Singleton.
     /// </remarks>
     class Customers
     {
@@ -37,6 +37,11 @@ namespace MyStore.Store
 
         //Required Functionality
         //question: what do if shared name?
+        /// <summary>
+        /// Tells the system that a customer exists.
+        /// </summary>
+        /// <param name="c">A new customer object not already registered.</param>
+        /// <returns>The argument.</returns>
         public Customer RegisterCustomer(Customer c)
         {
             if (!AllCustomers.Contains(c))
@@ -46,12 +51,25 @@ namespace MyStore.Store
             return c;
         }
 
-
+        /// <summary>
+        /// Tells the system that a customer exists.
+        /// </summary>
+        /// <param name="name">The name of a customer.</param>
+        /// <param name="DefaultStore">(Optional) the defualt location for a customer.</param>
+        /// <returns>The new customer object.</returns>
         public Customer RegisterCustomer(Name name, Location DefaultStore = null)
         {
             return RegisterCustomer(new Customer(name, DefaultStore));
         }
 
+        /// <summary>
+        /// Tells the system that a customer exists.
+        /// </summary>
+        /// <param name="first">The customer's first name.</param>
+        /// <param name="last">The customer's last name.</param>
+        /// <param name="middle">The customer's middle initial.</param>
+        /// <param name="DefaultStore">The customer's home store.</param>
+        /// <returns>The new customer object</returns>
         public Customer RegisterCustomer(string first, string last, char? middle = null, Location DefaultStore = null)
         {
             return RegisterCustomer(new Name(first, last, middle), DefaultStore);
@@ -59,6 +77,11 @@ namespace MyStore.Store
 
         //required functionality
         //get customer by name
+        /// <summary>
+        /// Get's a customer given their name.
+        /// </summary>
+        /// <param name="customerName">The Name of a customer.</param>
+        /// <returns>The customer.</returns>
         public Customer GetCustomer(Name customerName)
         {
             Customer desired = null;
