e2ade8b17f43d6d55f68c91a0275b9df150e2209
peter346 <peter346@wwu.edu>
2020-11-10 08:10:08 -0800

    Fixed Order History

--- MyStore.Store/OrderHistory.cs
+++ MyStore.Store/OrderHistory.cs
@@ -4,14 +4,30 @@ using System.Text;
 
 namespace MyStore.Store
 {
-    class OrderHistory
+    public class OrderHistory
     {
+        private List<IOrder> _orderHistory;
         //order history for a thing
-        List<IOrder> orderhistory;
+        public IReadOnlyCollection<IOrder> orderhistory 
+        { 
+            get
+            {
+                return _orderHistory.AsReadOnly();
+            } 
+        }
 
-        public void AddOrder(IOrder o)
+        /// <summary>
+        /// Assumes
+        /// </summary>
+        /// <param name="o"></param>
+        internal void AddOrder(IOrder o)
         {
+            _orderHistory.Add(o);
+        }
 
+        public OrderHistory()
+        {
+            _orderHistory = new List<IOrder>();
         }
     }
 }
