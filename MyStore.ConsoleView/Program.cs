92803f56154e60b37b6bbbc2ad49c67ebab8c91b
peter346 <peter346@wwu.edu>
2020-11-10 12:32:06 -0800

    Setup to test file writing

--- MyStore.ConsoleView/Program.cs
+++ MyStore.ConsoleView/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MyStore.Store;
+using MyStore.Store.Serialization;
 
 namespace MyStore.ConsoleView
 {
@@ -7,6 +9,41 @@ namespace MyStore.ConsoleView
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            StoreCatalogue.Instance.RegisterItem("Item1", 1);
+            StoreCatalogue.Instance.RegisterItem("Item2", 2);
+            StoreCatalogue.Instance.RegisterItem("Item3", 3);
+            StoreCatalogue.Instance.RegisterItem("Item4", 4);
+            StoreCatalogue.Instance.RegisterItem("Item5", 5);
+
+
+            Locations.Instance.RegisterLocation("Store1");
+            Locations.Instance.GetLocation("Store1").AddInventory("Item1", 50);
+            Locations.Instance.GetLocation("Store1").AddInventory("Item2", 1);
+            Locations.Instance.GetLocation("Store1").AddInventory("Item3", 10);
+
+            Locations.Instance.RegisterLocation("Store2");
+            Locations.Instance.GetLocation("Store2").AddInventory("Item4", 50);
+            Locations.Instance.GetLocation("Store2").AddInventory("Item5", 60);
+            Locations.Instance.GetLocation("Store2").AddInventory("Item2", 25);
+
+            Customer c = Customers.Instance.RegisterCustomer("Daniel", "last", 'm');
+            Customers.Instance.RegisterCustomer("Randel", "last", 'n');
+            Customers.Instance.RegisterCustomer("Daniel", "last");
+
+            Console.WriteLine("Creating first order");
+            Order o = Locations.Instance.GetLocation("Store1").CreateNewOrder("Item1", 10, c);
+            o.FinallizeOrder();
+
+
+            o = Locations.Instance.GetLocation("Store2").CreateNewOrder("Item5", 1, c);
+            o.EditOrderAmounts("Item2", 5);
+            o.FinallizeOrder();
+
+
+            XMLWriter writer = new XMLWriter();
+            writer.WriteAllData();
         }
     }
 }
