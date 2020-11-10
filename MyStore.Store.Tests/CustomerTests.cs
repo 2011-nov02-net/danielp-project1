34ec9516e367d304666f85362f0f966085bc08bc
peter346 <peter346@wwu.edu>
2020-11-09 16:44:59 -0800

    Created Customer Tests class

---
+++ MyStore.Store.Tests/CustomerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MyStore.Store.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void test1()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
