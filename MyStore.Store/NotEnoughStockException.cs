697bb9a8d7e5774e70e4e006a0c9fcb013a4ca8e
peter346 <peter346@wwu.edu>
2020-11-09 17:33:10 -0800

    Created new exception for not enough stock

---
+++ MyStore.Store/NotEnoughStockException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Store
+{
+    public class NotEnoughStockException : ArgumentException
+    {
+        public NotEnoughStockException(string message) : base(message)
+        {
+        }
+    }
+}
