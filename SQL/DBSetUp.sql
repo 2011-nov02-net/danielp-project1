2daeef7e43110d7bc010b9763ca41b91a6b6294a
peter346 <peter346@wwu.edu>
2020-11-13 07:09:04 -0800

    Added Plan for Tables

---
+++ SQL/DBSetUp.sql
@@ -0,0 +1,29 @@
+
+--https://github.com/2011-nov02-net/trainer-code/wiki/Project-0-requirements
+-- create tables
+
+
+-- orders table
+	-- order id
+	-- customer id
+	-- store id
+-- order-items table
+	-- order id
+	-- item id
+	-- quantity
+-- customers table
+	-- first name
+	-- middle initial
+	-- last name
+	-- default store
+-- store table
+	-- store id
+	-- location name
+-- store invintory table
+	-- store id
+	-- item id
+	-- quantity
+-- item table
+	-- item id
+	-- item name
+	-- item price
