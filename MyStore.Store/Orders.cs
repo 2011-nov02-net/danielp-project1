bdcf659a8ddbcba40e15b299f7f33f9d2d4bce51
peter346 <peter346@wwu.edu>
2020-11-13 21:10:37 -0800

    Started order refactoring

---
+++ MyStore.Store/Orders.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MyStore.Store
+{
+    //TODO: Replace order history classes.
+    /// <summary>
+    /// singleton
+    /// </summary>
+    class Orders
+    {
+        private List<IOrder> AllOrders;
+        private static Orders _instance;
+
+        public static Orders Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Orders();
+                }
+                return _instance;
+            }
+        }
+
+        private Orders()
+        {
+            AllOrders = new List<IOrder>();
+        }
+
+        public IEnumerable<IOrder> GetOrdersByCustomer(Customer c)
+        {
+            return AllOrders.AsReadOnly().Where(ord => ord.Customer == c); 
+        }
+
+        public IEnumerable<IOrder> GetOrdersByLocation(Location l)
+        {
+            return AllOrders.AsReadOnly().Where(ord => ord.OrderLoc == l);
+        }
+    }
+}
+}
