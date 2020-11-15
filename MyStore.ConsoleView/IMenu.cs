5e52b8250b384746dae10417f5e234acb9916bb3
peter346 <peter346@wwu.edu>
2020-11-14 16:46:45 -0800

    REFACTOR: Renamed Menu interface

---
+++ MyStore.ConsoleView/IMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.ConsoleView
+{
+    /// <summary>
+    /// Represents the current state of the program.
+    /// </summary>
+    interface IMenu
+    {
+        //Display
+        /// <summary>
+        /// Handles input for the current menu, displays anything, then 
+        /// </summary>
+        /// <returns>Returns the next IMenu (state), or null to exit the program.</returns>
+        IMenu DisplayMenu();
+    }
+}
