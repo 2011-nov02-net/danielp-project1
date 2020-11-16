1d2f41aa72ebfe652bd17c94231be29f19088e65
peter346 <peter346@wwu.edu>
2020-11-15 18:26:39 -0800

    Implemented GetCustomerByName

--- MyStore.DataModel/DBRepository.cs
+++ MyStore.DataModel/DBRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using MyStore.Store;
 using Microsoft.EntityFrameworkCore;
-
+using System.Linq;
 
 namespace MyStore.DataModel
 {
@@ -22,9 +22,47 @@ namespace MyStore.DataModel
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Get a customer from the db by name, and register it with the data model assuming it doesn't already
+        /// exist in the model.
+        /// </summary>
+        /// <param name="name">The name of the customer.</param>
+        /// <returns>The customer created (or returned) by the Customers object in the datamodel.</returns>
         public Store.Customer GetCustomerByName(Name name)
         {
-            throw new NotImplementedException();
+            using Project0DBContext DBContext = new Project0DBContext(dbContextOptions);
+
+            Customer DBCustomer;
+            if (name.MiddleInitial == null)
+            {
+                DBCustomer = DBContext.Customers.Where(cust =>
+                    name.First.StartsWith(cust.FirstName)
+                    && name.Last.StartsWith(cust.LastName)
+                    && null == cust.MiddleInitial)
+                    .SingleOrDefault();
+            } else
+            {
+                DBCustomer = DBContext.Customers.Where(cust =>
+                    name.First.StartsWith(cust.FirstName)
+                    && name.Last.StartsWith(cust.LastName)
+                    && name.MiddleInitial.ToString() == cust.MiddleInitial)
+                    .Take(1).SingleOrDefault();
+            }
+
+            if (Customers.Instance.HasCustomer(name))
+            {
+                //something weird happened probably. Expecting customers to be gotten from 
+                //the model first before checking DB.
+                Console.Error.WriteLine($"Warning: Customer {name} already existed in the model");
+                return Customers.Instance.GetCustomer(name);
+            } else
+            {
+                if(DBCustomer.StoreLocation != null)
+                {
+                    return Customers.Instance.RegisterCustomer(name, Locations.Instance.GetOrRegisterLocation(DBCustomer.StoreLocation));
+                }
+                return Customers.Instance.RegisterCustomer(name, Locations.Instance.GetOrRegisterLocation(DBCustomer.StoreLocation));
+            }
         }
 
         public IEnumerable<Store.Customer> GetCustomers()
