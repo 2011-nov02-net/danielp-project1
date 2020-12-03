2e37a2db4ca6a1e52768b05a0c882e1961b8d6f1
Noelwiz <noelwiz1@gmail.com>
2020-12-03 07:50:59 -0800

    Fixed build error

--- MyStore.ConsoleView/Program.cs
+++ MyStore.ConsoleView/Program.cs
@@ -4,7 +4,6 @@ using System.IO;
 using Microsoft.EntityFrameworkCore;
 using MyStore.DataModel;
 using MyStore.Store;
-using MyStore.Store.Serialization;
 
 namespace MyStore.ConsoleView
 {
@@ -176,40 +175,6 @@ namespace MyStore.ConsoleView
 
             optionsBuilder.UseSqlServer(connectionStr);
             return optionsBuilder.Options;
-        }
-
-        private static void Setup()
-        {
-
-            StoreCatalogue.Instance.RegisterItem("Item1", 1);
-            StoreCatalogue.Instance.RegisterItem("Item2", 2);
-            StoreCatalogue.Instance.RegisterItem("Item3", 3);
-            StoreCatalogue.Instance.RegisterItem("Item4", 4);
-            StoreCatalogue.Instance.RegisterItem("Item5", 5);
-
-
-            Locations.Instance.RegisterLocation("Store1");
-            Locations.Instance.GetLocation("Store1").AddInventory("Item1", 50);
-            Locations.Instance.GetLocation("Store1").AddInventory("Item2", 1);
-            Locations.Instance.GetLocation("Store1").AddInventory("Item3", 10);
-
-            Locations.Instance.RegisterLocation("Store2");
-            Locations.Instance.GetLocation("Store2").AddInventory("Item4", 50);
-            Locations.Instance.GetLocation("Store2").AddInventory("Item5", 60);
-            Locations.Instance.GetLocation("Store2").AddInventory("Item2", 25);
-
-            Store.Customer c = Customers.Instance.RegisterCustomer("Daniel", "last", 'm');
-            Customers.Instance.RegisterCustomer("Randel", "last", 'n');
-            Customers.Instance.RegisterCustomer("Daniel", "last");
-
-            Console.WriteLine("Creating first order");
-            Store.Order o = Locations.Instance.GetLocation("Store1").CreateNewOrder("Item1", 10, c);
-            o.FinallizeOrder();
-
-
-            o = Locations.Instance.GetLocation("Store2").CreateNewOrder("Item5", 1, c);
-            o.EditOrderAmounts("Item2", 5);
-            o.FinallizeOrder();
         }
     }
 }
