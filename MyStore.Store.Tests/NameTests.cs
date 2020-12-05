80a4decb969faf4937cb3088031359b76c1249b2
Noelwiz <noelwiz1@gmail.com>
2020-12-04 20:17:13 -0800

    Added a new names constructor

---
+++ MyStore.Store.Tests/NameTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using MyStore.Store;
+
+namespace MyStore.Store.Tests
+{
+    public class NameTests
+    {
+
+        [Theory]
+        [InlineData("Someone T anyone","Someone", 'T',"anyone")]
+        [InlineData("          Someone T        anyone", "Someone", 'T', "anyone")]
+        public void RegisterLocation(String tostrName, string first, char? middle, string last)
+        {
+            //Arrange
+            Name othername = new Name(first, last, middle);
+
+            //Act
+            Name testname = new Name(tostrName);
+            //Assert
+            Assert.Equal(first, testname.First);
+
+            Assert.Equal(middle, testname.MiddleInitial);
+
+            Assert.Equal(last, testname.Last);
+
+            Assert.Equal(othername, testname);
+        }
+    }
+}
--- MyStore.Store/Name.cs
+++ MyStore.Store/Name.cs
@@ -1,15 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyStore.Store
 {
+    /// <summary>
+    /// A struct used to store customer names for easy accsess and identififcation.
+    /// </summary>
     public struct Name : IComparer<Name>
     {
+        /// <summary>
+        /// A first name
+        /// </summary>
         public string First { get; }
+        /// <summary>
+        /// A middle initial
+        /// </summary>
         public char? MiddleInitial { get; }
+        /// <summary>
+        /// A last name
+        /// </summary>
         public string Last { get; }
 
+        /// <summary>
+        /// Convert a Name.tostring() back into a name.
+        /// </summary>
+        /// <param name="namestr">A name that's been transformed into a string.</param>
+        public Name(string namestr)
+        {
+            char[] splitchar = new char[1];
+            splitchar[0] = ' ';
+            List<string> nameparts = namestr.Split(splitchar, StringSplitOptions.RemoveEmptyEntries).ToList() ;
+
+            foreach (var str in nameparts)
+            {
+                if (string.IsNullOrWhiteSpace(str))
+                {
+                    nameparts.Remove(str);
+                    str.Trim(); 
+                }
+            }
+
+            /*
+            for (int i = 0; i < nameparts.Count; i++)
+            {
+                nameparts[i].Trim();
+            }
+            */
+
+            //default value
+            MiddleInitial = null;
+
+            if(nameparts.Count == 2 || nameparts.Count == 3)
+            {
+                First = nameparts[0].Trim();
+
+                if(nameparts.Count == 3)
+                {
+                    //middle initial 
+                    if(nameparts[1].Length > 1)
+                    {
+                        Console.Error.WriteLine("Warning: middle name has more than 1 character in the name constructor.");
+                        Console.Error.WriteLine($"Origional name: {namestr}. \t Percieved middle name: {nameparts[1]}");
+
+                        MiddleInitial = nameparts[1][0];
+                    } else if(nameparts[1].Length == 1)
+                    {
+                        MiddleInitial = nameparts[1][0];
+                    } else if (nameparts[1].Length < 1)
+                    {
+                        Console.Error.WriteLine("More than two entries in the name, but the second one is empty.");
+                        throw new ArgumentException("Empty string entries not removed.");
+                    }
+
+                    Last = nameparts[2];
+                } else
+                {
+                    Last = nameparts[1];
+                }
+            } else
+            {
+                throw new ArgumentException("Invalid name, must have at least a first and a last name.");
+            }
+        }
+
+
+        /// <summary>
+        /// Create a name with no middle initial.
+        /// </summary>
+        /// <param name="first">The person's first name.</param>
+        /// <param name="last">The person's last name.</param>
         public Name(string first, string last)
         {
             First = first;
@@ -17,6 +98,12 @@ namespace MyStore.Store
             MiddleInitial = null;
         }
 
+        /// <summary>
+        /// Create a name with a first and last name and optionally a middle initial.
+        /// </summary>
+        /// <param name="first">The person's first name.</param>
+        /// <param name="last">The person's last name.</param>
+        /// <param name="middle">The person's middle initial.</param>
         public Name(string first, string last, char? middle = null)
         {
             First = first;
@@ -25,6 +112,12 @@ namespace MyStore.Store
         }
 
 
+        /// <summary>
+        /// Compares two names to eachother
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Compare(Name x, Name y)
         {
             if(x.First == y.First)
@@ -66,11 +159,20 @@ namespace MyStore.Store
             }
         }
 
+        /// <summary>
+        /// Converts a name to a string
+        /// </summary>
+        /// <returns>The name in a regular format.</returns>
         public override string ToString()
         {
             return $"{First,20} {MiddleInitial?.ToString() ?? " "} {Last, 20}";
         }
 
+        /// <summary>
+        /// Compares the equality of two names.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj)
         {
             if(obj is Name)
