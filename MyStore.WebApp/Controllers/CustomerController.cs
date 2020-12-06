e5b4c3fbf390339874fad2c2f854344d5ad3a7c3
Noelwiz <noelwiz1@gmail.com>
2020-12-05 20:16:38 -0800

    Pulled out duplicate code, View work

--- MyStore.WebApp/Controllers/CustomerController.cs
+++ MyStore.WebApp/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyStore.DataModel;
@@ -12,8 +13,14 @@ namespace MyStore.WebApp.Controllers
 {
     public class CustomerController : Controller
     {
-        //GETl  /Customer/Choose
-        // log into a customer
+        //GET  /Customer/Choose
+        // view all customers, search by name.
+        /// <summary>
+        /// Display a list of all customers that you can search by name
+        /// </summary>
+        /// <param name="repo">The repository of info, pulled from DB</param>
+        /// <param name="searchString">Filters list of customers to only the ones with the searchString in their name</param>
+        /// <returns></returns>
         public ActionResult Choose([FromServices] IDbRepository repo, string searchString)
         {
             /* List of customers as CustomerViewModel in an IEnumerable*/
@@ -37,27 +44,27 @@ namespace MyStore.WebApp.Controllers
         // GET: Customer/Create
         // create a customer
         public ActionResult Create([FromServices] IDbRepository repo)
-        {
-            List<String> stores = new List<string>();
-
-            stores.Add("None");
-
-            foreach ( var store in repo.GetLocations().ToList())
-            {
-                stores.Add(store.Where);
-            }
-
-           
-
-            ViewData["Stores"] = stores;
+        {       
+            ViewData["Stores"] = GetStoreNames(repo);
             return View();
         }
 
         // GET: Customer/Details/5
         // view order history and other details
-        public ActionResult Details(int id)
+        public ActionResult Details([FromServices] IDbRepository repo, string customerName)
         {
-            return View();
+            if (string.IsNullOrWhiteSpace(customerName))
+            {
+                //todo: set error thing to display on the view, could not find customer
+
+                return View(nameof(Choose));
+            } else
+            {
+                repo.GetCustomerByName(new Name(customerName));
+
+
+                return View();
+            }
         }
 
 
@@ -67,15 +74,16 @@ namespace MyStore.WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([FromServices] IDbRepository repo, IFormCollection collection)
         {
+            //TODO: check the form status for errors.
+
             try
             {
-                Console.WriteLine(collection["HomeStore"]);
-                Console.WriteLine(collection["Name"]);
 
 
                 Store.Location homestore = null;
                 if (collection["HomeStore"] != "None")
                 {
+                    Console.WriteLine(collection["HomeStore"]);
                     homestore = repo.GetLocation(collection["HomeStore"]);
                 }
 
@@ -87,6 +95,7 @@ namespace MyStore.WebApp.Controllers
                 }
 
                 Name custname = new Name(collection["FirstName"], collection["LastName"], middle);
+                Console.WriteLine(custname.ToString());
 
                 if (Customers.Instance.HasCustomer(custname))
                 {
@@ -113,8 +122,22 @@ namespace MyStore.WebApp.Controllers
         //TODO: decide if we want customers to be editable based on info we wind up displayed
         // could be to change name, but that is a big change in the DB, and not a req
         // GET: Customer/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit([FromServices] IDbRepository repo, string customerName)
         {
+            if (string.IsNullOrWhiteSpace(customerName))
+            {
+                Console.Error.WriteLine("Bad Customer Name given.");
+                return View(nameof(Choose));
+            } else
+            {
+                //todo: fix so that this will work, either split up first/middle/last or do something else, or just 
+                //accept that space will be converted to +
+                customerName = HttpUtility.UrlDecode(customerName);
+                repo.GetCustomerByName(new Name(customerName));
+            }
+            
+
+            ViewData["Stores"] = GetStoreNames(repo);
             return View();
         }
 
@@ -132,29 +155,23 @@ namespace MyStore.WebApp.Controllers
                 return View();
             }
         }
-        
 
-        /* probably wont let deletion happen because it will mess with orders in the DB
-        // GET: Customer/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
-        // POST: Customer/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        /// <summary>
+        /// Get a list of all store names, and convert them to something displayable in a view. 
+        /// </summary>
+        /// <param name="repo">DB Repository.</param>
+        /// <returns>List of strings, representing all existing store names.</returns>
+        private List<String> GetStoreNames(IDbRepository repo)
         {
-            try
+            List<String> stores = new List<string>();
+
+            foreach (var store in repo.GetLocations().ToList())
             {
-                return RedirectToAction(nameof(Index));
+                stores.Add(store.Where);
             }
-            catch
-            {
-                return View();
-            }
+
+            return stores;
         }
-        */
     }
 }
