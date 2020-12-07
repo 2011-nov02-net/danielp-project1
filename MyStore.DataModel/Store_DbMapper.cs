06419c7a98619ccdc0e6b09ecca2f1243dfa9aee
Noelwiz <noelwiz1@gmail.com>
2020-12-06 18:14:02 -0800

    Mapped Orders to the Model

--- MyStore.DataModel/Store_DbMapper.cs
+++ MyStore.DataModel/Store_DbMapper.cs
@@ -6,8 +6,13 @@ using MyStore.Store.Exceptions;
 
 namespace MyStore.DataModel
 {
-    class Db_StoreMapper
+    static class Db_StoreMapper
     {
+        /// <summary>
+        /// Takes a datamodel customer and turns it into a store customer.
+        /// </summary>
+        /// <param name="DbCustomer">A data model customer to be added to the model</param>
+        /// <returns>A Model customer</returns>
         public static Store.Customer MapCustomerToStore(DataModel.Customer DbCustomer)
         {
             if (DbCustomer != null)
@@ -35,7 +40,12 @@ namespace MyStore.DataModel
             }
         }
 
-
+        /// <summary>
+        /// Takes a DB Location and maps it to a store Location. Will add
+        /// to the model if not already there.
+        /// </summary>
+        /// <param name="DbLocation">The DB representation of the location.</param>
+        /// <returns>The Model representation of the location.</returns>
         public static Store.Location MapLocationToStore(DataModel.Location DbLocation)
         {
             Store.Location StoreLocation;
@@ -43,7 +53,7 @@ namespace MyStore.DataModel
             // if not, add to model
             if (!Locations.Instance.HasLocation(lname))
             {
-                StoreLocation = Store.Locations.Instance.RegisterLocation(lname);
+                StoreLocation = Locations.Instance.RegisterLocation(lname);
 
                 //get invintory
                 foreach (Invintory inv in DbLocation.Invintories)
@@ -57,8 +67,6 @@ namespace MyStore.DataModel
                         StoreCatalogue.Instance.RegisterItem(inv.ItemName, inv.ItemNameNavigation.ItemPrice);
                         StoreLocation.AddInventory(inv.ItemName, inv.Quantity);
                     }
-
-
                 }
             }
             else
@@ -68,6 +76,63 @@ namespace MyStore.DataModel
 
             return StoreLocation;
         }
+
+
+
+        /// <summary>
+        /// Create a model order and add it to the Model based of a DB order
+        /// </summary>
+        /// <remarks>
+        /// Assumes that this order is confirmed unique by the caller, and should be added.
+        /// Will be added as a Historic order. Takes a name so that all the customers details will
+        /// be recoreded if they are not in the Model.
+        /// </remarks>
+        /// <param name="LocationOrder_DB">The DB order item</param>
+        /// <param name="customername">The name of the customer placing the order</param>
+        public static void MapAndAddOrderToModel(Order LocationOrder_DB)
+        {
+
+            ICollection<ItemCount> orderitems = new List<ItemCount>();
+            foreach (OrderItem oi in LocationOrder_DB.OrderItems)
+            {
+                try
+                {
+                    orderitems.Add(new ItemCount(oi.Quantity, oi.Item.ItemName));
+                }
+                catch (ItemNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    StoreCatalogue.Instance.RegisterItem(oi.Item.ItemName, oi.Item.ItemPrice);
+                    // retry 
+                    orderitems.Add(new ItemCount(oi.Quantity, oi.Item.ItemName));
+                }
+
+            }           
+
+            try
+            {
+                Store.Orders.Instance.CreateAndAddPastOrder(LocationOrder_DB.StoreLocation,
+                    getCustomerName(LocationOrder_DB.Customer),
+                    LocationOrder_DB.OrderTime,
+                    orderitems,
+                    LocationOrder_DB.OrderTotal);
+            } catch (CustomerNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                //load the customer to the store
+                Store.Customer storecust = MapCustomerToStore(LocationOrder_DB.Customer);
+                //try and load the order to the model
+                Store.Orders.Instance.CreateAndAddPastOrder(LocationOrder_DB.StoreLocation,
+                    storecust.CustomerName,
+                    LocationOrder_DB.OrderTime,
+                    orderitems,
+                    LocationOrder_DB.OrderTotal);
+            }
+            
+        }
+
+
 
         public static Name getCustomerName(DataModel.Customer c)
         {
